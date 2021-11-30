06368026b25ff3e3375179d199d7d33a093f9bc2
makotot <m.tateno@prog-8.com>
2021-11-30 18:07:01 +0900

    make mf works

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin")
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+  devServer: {
+    port: 3002,
+  },
+  output: {
+    publicPath: "auto",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ModuleFederationPlugin({
+      name: 'main',
+      filename: 'remoteEntry.js',
+      remotes: {
+        'mf_header': 'mf_header@http://localhost:3001/remoteEntry.js',
+      },
+    })
+  ],
+}
