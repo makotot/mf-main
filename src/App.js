07ed4c4c1cbb7a43e25e21115470fa15424cc7b4
makotot <makoto.ttn@gmail.com>
2021-12-02 01:03:03 +0900

    add shared ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import {Header} from 'mf_header/Header'
+import { Header } from 'mf_header/Header'
+import { Button } from 'mf_shared_ui/Button'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div>main app</div>
+      <div>
+        <Button>btn from shared ui</Button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,38 @@
 const path = require('path')
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 
 module.exports = {
-  entry: path.resolve(__dirname, "./src/index.js"),
+  entry: path.resolve(__dirname, './src/index.js'),
   devServer: {
     port: 3002,
   },
   output: {
-    publicPath: "auto",
+    publicPath: 'auto',
   },
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
     new ModuleFederationPlugin({
       name: 'main',
       filename: 'remoteEntry.js',
       remotes: {
-        'mf_header': 'mf_header@http://localhost:3001/remoteEntry.js',
+        mf_header: 'mf_header@http://localhost:3001/remoteEntry.js',
+        mf_shared_ui: 'mf_shared_ui@http://localhost:3003/remoteEntry.js',
       },
-    })
+    }),
   ],
 }
